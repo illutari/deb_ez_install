2b74e4091388108b6e8ad72e77ec055015f8ecad
illutari <illutari@pm.me>
2025-09-28 17:21:48 -0400

    Fixed apt list

--- launcher.sh
+++ launcher.sh
@@ -154,8 +154,7 @@ install_apt_pkg() {
         pack_name=$install_pack
     fi
     # Check to see if package is installed
-    #if dpkg-query -W $install_pack >/dev/null 2>&1; then
-    if apt list --installed $install_pack >/dev/null 2>&1; then
+    if apt list --installed "$install_pack" 2>/dev/null | grep -q "\[installed\]"; then
         echo -e "${GR}${pack_name} is already installed!${NC}"
     else # Install package
         echo -e "${ORA}Installing ${pack_name}...${NC}"
